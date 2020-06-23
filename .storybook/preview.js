609e8863516b342d672bd084e9a3066df3927072
Stephen Niedzielski <stephen@niedzielski.com>
2020-06-23 14:54:29 -0600

    [dev] Suppress Vue.js Storybook build type reminder
    Disable the build type notification for Storybook documentation. This
    library requires Vue.js as a peer dependency but does not ship it. The
    Storybook documentation _does_ bundle Vue.js but it's probably better to
    offer the development build even in production.

---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+// Disable informational message in the browser console log:
+//
+//   You are running Vue in development mode.
+//   Make sure to turn on production mode when deploying for production.
+//   See more tips at https://vuejs.org/guide/deployment.html
+//
+// This setting only applies to documentation.
+Vue.config.productionTip = false;
