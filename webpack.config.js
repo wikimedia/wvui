f10127023e5f0a32cef910262646535ba5af0cbb
Anri Bolon <anri.b@speedandfunction.com>
2020-06-29 20:12:31 +0300

    Simple webpack bundling config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+/* eslint-env node */
+
 const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const TerserJSPlugin = require( 'terser-webpack-plugin' );
+const { VueLoaderPlugin } = require( 'vue-loader' );
 const webpack = require( 'webpack' );
 const path = require( 'path' );
 
@@ -7,6 +13,12 @@ const resolve = {
 	extensions: [ '.js', '.ts' ],
 	// Map @/ to src/. These should match tsconfig.json.
 	alias: { '@': path.resolve( __dirname, './src' ) }
+};
+
+const jsSourceMapExtention = '.map.json';
+
+const Chunk = {
+	Wvui: 'wvui'
 };
 
 /**
@@ -56,5 +68,96 @@ function plugins() {
 		new MiniCssExtractPlugin()
 	];
 }
+
+/**
+ * @param {string} _env
+ * @param {webpack.CliConfigOptions} argv
+ * @return {webpack.Configuration}
+ */
+module.exports = ( _env, argv ) => ( {
+	stats: {
+		all: false,
+		// Output a timestamp when a build completes. Useful when watching files.
+		builtAt: true,
+		errors: true,
+		warnings: true
+	},
+
+	resolve,
+
+	// Map of chunk names to entry files.
+	entry: {
+		[ Chunk.Wvui ]: './src/entries/wvui.ts'
+		// Other chunks are configured under optimization.
+	},
+
+	// Omit these external dependencies to be provided by the consumer.
+	externals: [ 'vue' ],
+
+	performance: {
+		// The default filter excludes map files but we rename ours. See T173491.
+		assetFilter: ( filename ) => !filename.endsWith( jsSourceMapExtention )
+	},
+
+	// Accurate source maps come at the expense of build time. The source map is intentionally
+	// exposed to users via sourceMapFilename for prod debugging. This goes against convention as
+	// this source code is publicly distributed.
+	devtool: argv.mode === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+
+	optimization: {
+		runtimeChunk: {
+			/**
+			 * @return {string}
+			 * */
+			name() {
+				return Chunk.Wvui;
+			}
+		},
+		// Enable CSS minification. Unfortunately, this overrides the default JavaScript
+		// minification so it must be re-enabled with the TerserJSPlugin. The default processor is
+		// cssnano which uses postcss.
+		// https://github.com/webpack-contrib/mini-css-extract-plugin#minimizing-for-production
+		// https://github.com/NMFR/optimize-css-assets-webpack-plugin
+		// https://cssnano.co
+		minimizer: argv.mode === 'production' ? [
+			new TerserJSPlugin(),
+			new OptimizeCSSAssetsPlugin( {
+				cssProcessorOptions: {
+					// Keep sourceMappingURL comments in the output CSS.
+					// https://github.com/postcss/postcss/blob/master/docs/source-maps.md
+					map: { annotation: true }
+				}
+			} )
+		] : []
+	},
+
+	output: {
+		sourceMapFilename: `[file]${jsSourceMapExtention}`,
+		// Set the name to avoid possible Webpack runtime collisions of globals with other Webpack
+		// runtimes. See https://webpack.js.org/configuration/output/#outputuniquename.
+		library: 'wvui',
+		libraryTarget: 'umd',
+		// https://github.com/webpack/webpack/issues/6525
+		globalObject: 'this'
+	},
+
+	module: {
+		rules: [
+			...rules( argv.mode ),
+			// Process single-file components (SFCs). This match loader extensions to the SFC
+			// language attributes.
+			{ test: /\.vue$/, use: 'vue-loader' }
+		]
+	},
+
+	plugins: [
+		new CleanWebpackPlugin( {
+			// Don't delete the ES5 linter config.
+			cleanOnceBeforeBuildPatterns: [ '**/*', '!.eslintrc.js' ]
+		} ),
+		...plugins(),
+		new VueLoaderPlugin()
+	]
+} );
 
 module.exports.commonConfig = { resolve, rules, plugins };
