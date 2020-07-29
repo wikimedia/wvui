632dfa7cd1a665352d834f8a0a02f9f5f38a4d53
annet <atomasevich@wikimedia.org>
2020-07-29 17:18:55 -0400

    [dev] Add icons and Icon component to main entry

--- src/entries/wvui.ts
+++ src/entries/wvui.ts
@@ -1,8 +1,13 @@
 import WvuiButton from '@/components/button/Button.vue';
+import WvuiIcon from '@/components/input/Input.vue';
 import WvuiInput from '@/components/input/Input.vue';
 
+export const version = VERSION;
+
+export * as icons from '@/themes/icons';
+
 export default {
-	version: VERSION,
 	WvuiButton,
+	WvuiIcon,
 	WvuiInput
 };
