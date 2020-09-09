775b5f85350b630dcdeda1338c9f2559dc1b93de
Stephen Niedzielski <stephen@niedzielski.com>
2020-09-09 08:17:54 -0600

    [build] Minimize whitespace in Vue Webpack config

--- .webpack/config.js
+++ .webpack/config.js
@@ -102,7 +102,23 @@ function config( argv, name, entry ) {
 				...rules( argv.mode ),
 				// Process single-file components (SFCs). This matches loader extensions to the SFC
 				// language attributes.
-				{ test: /\.vue$/, use: 'vue-loader' }
+				{
+					test: /\.vue$/,
+					use: {
+						loader: 'vue-loader',
+						options: {
+							compilerOptions: {
+								// Any whitespace or comment differences between tags causes client
+								// hydration to fail. When whitespace is preserved, it is difficult as a
+								// client to anticipate where these occur as Vue.js is not always used
+								// to generate the HTML server-side. Remove all whitespace so clients
+								// can structure their elements deterministically. Note: this only
+								// appears to work correctly for production builds.
+								whitespace: 'condense'
+							}
+						}
+					}
+				}
 			]
 		},
 
